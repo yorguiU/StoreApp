ba316f707793396f3640a03e178b237d709903d8
Kamoka2022 <miguelangelserranoaraque@gmail.com>
2025-04-25 15:56:49 -0500

    Config-Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,4 +66,7 @@ dependencies {
     //Esta es la libreria para cargae images de internet
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+
+    //firebase
+    implementation(libs.firebase.bom)
 }
